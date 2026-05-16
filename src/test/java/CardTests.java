46564a92c2e23b2afccd622cda6cd75e95761575
danielachangnu <danielachangkim2028@u.northwestern.edu>
2026-05-15 21:35:05 -0500

    updated card class w simplified changes

--- src/test/java/CardTests.java
+++ src/test/java/CardTests.java
@@ -5,50 +5,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CardTests {
 
     @Test
-    void constructor_NullTypeNullAction_ThrowsIllegalArgumentException() {
+    void constructor_NullTypeThrowsIllegalArgumentException() {
         CardType type = null;
-        Action action =  null;
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Card(type, action);
-        });
-
-        assertEquals("need a card type and an action!", exception.getMessage());
-    }
-
-    @Test
-    void constructor_NullTypeAndAction_ThrowsIllegalArgumentException() {
-        CardType type = null;
-        Action action =  new Action() {};;
-
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Card(type, action);
+            new Card(type);
         });
 
         assertEquals("need a card type!", exception.getMessage());
     }
 
-    @Test
-    void constructor_ValidTypeNullAction_CreatesCard() {
-        CardType type = CardType.TEST_TYPE;
-        Action action = null;
-
-        Card card = new Card(type, action);
-
-        assertEquals(CardType.TEST_TYPE, card.getType());
-        assertNull(card.getAction());
-    }
 
     @Test
-    void constructor_ValidTypeAndAction_CreatesCard() {
+    void constructor_ValidType_CreatesCard() {
         CardType type = CardType.TEST_TYPE;
-        Action action = new Action() {};
 
-        Card card = new Card(type, action);
+        Card card = new Card(type);
 
         assertEquals(CardType.TEST_TYPE, card.getType());
-        assertNotNull(card.getAction());
-        assertEquals(action, card.getAction());
     }
 }
 
