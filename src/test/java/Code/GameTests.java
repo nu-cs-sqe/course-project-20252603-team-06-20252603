14bcef6179ac417639f13a321d35ff5a23712223
AudreyPaleczny2028 <audreypaleczny2028@u.northwestern.edu>
2026-05-25 00:46:08 -0500

    updated GameTests so the previous setup tasks work with the other updated classes (a previous commit was updating the constructor tests)

--- src/test/java/Code/GameTests.java
+++ src/test/java/Code/GameTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,143 +32,53 @@ public class GameTests {
         }
     }
 
-//    @Test
-//    public void setup_TwoPlayers_DealsOneDefuseAndSevenCardsToEachPlayer() {
-//        Deck mockDeck = EasyMock.createMock(Deck.class);
-//        Player mockP1 = EasyMock.createMock(Player.class);
-//        Player mockP2 = EasyMock.createMock(Player.class);
-//        Card mockDefuse = EasyMock.createMock(Card.class);
-//        Card mockCard = EasyMock.createMock(Card.class);
-//
-//        EasyMock.expect(mockDeck.count()).andStubReturn(100);
-//        EasyMock.expect(mockDeck.drawDefuse()).andReturn(mockDefuse).times(2);
-//        EasyMock.expect(mockDeck.draw()).andReturn(mockCard).times(14);
-//
-//        mockP1.addCard(mockDefuse);
-//        EasyMock.expectLastCall().once();
-//        mockP1.addCard(mockCard);
-//        EasyMock.expectLastCall().times(7);
-//        mockP2.addCard(mockDefuse);
-//        EasyMock.expectLastCall().once();
-//        mockP2.addCard(mockCard);
-//        EasyMock.expectLastCall().times(7);
-//
-//        mockDeck.insert(EasyMock.anyObject(Card.class));
-//        EasyMock.expectLastCall().anyTimes();
-//        mockDeck.shuffle();
-//        EasyMock.expectLastCall().anyTimes();
-//
-//        EasyMock.replay(mockDeck, mockP1, mockP2, mockDefuse, mockCard);
-//
-//        new Game(mockDeck, List.of(mockP1, mockP2)).setup();
-//
-//        EasyMock.verify(mockDeck, mockP1, mockP2);
-//    }
-//
-//    @Test
-//    public void setup_ThreePlayers_DealsOneDefuseAndSevenCardsToEachPlayer() {
-//        Deck mockDeck = EasyMock.createMock(Deck.class);
-//        Player mockP1 = EasyMock.createMock(Player.class);
-//        Player mockP2 = EasyMock.createMock(Player.class);
-//        Player mockP3 = EasyMock.createMock(Player.class);
-//        Card mockDefuse = EasyMock.createMock(Card.class);
-//        Card mockCard = EasyMock.createMock(Card.class);
-//
-//        EasyMock.expect(mockDeck.count()).andStubReturn(100);
-//        EasyMock.expect(mockDeck.drawDefuse()).andReturn(mockDefuse).times(3);
-//        EasyMock.expect(mockDeck.draw()).andReturn(mockCard).times(21);
-//
-//        mockP1.addCard(mockDefuse);
-//        EasyMock.expectLastCall().once();
-//        mockP1.addCard(mockCard);
-//        EasyMock.expectLastCall().times(7);
-//        mockP2.addCard(mockDefuse);
-//        EasyMock.expectLastCall().once();
-//        mockP2.addCard(mockCard);
-//        EasyMock.expectLastCall().times(7);
-//        mockP3.addCard(mockDefuse);
-//        EasyMock.expectLastCall().once();
-//        mockP3.addCard(mockCard);
-//        EasyMock.expectLastCall().times(7);
-//
-//        mockDeck.insert(EasyMock.anyObject(Card.class));
-//        EasyMock.expectLastCall().anyTimes();
-//        mockDeck.shuffle();
-//        EasyMock.expectLastCall().anyTimes();
-//
-//        EasyMock.replay(mockDeck, mockP1, mockP2, mockP3, mockDefuse, mockCard);
-//
-//        new Game(mockDeck, List.of(mockP1, mockP2, mockP3)).setup();
-//
-//        EasyMock.verify(mockDeck, mockP1, mockP2, mockP3);
-//    }
-//
-//    @Test
-//    public void setup_FivePlayers_DealsOneDefuseAndSevenCardsToEachPlayer() {
-//        Deck mockDeck = EasyMock.createMock(Deck.class);
-//        Player mockP1 = EasyMock.createMock(Player.class);
-//        Player mockP2 = EasyMock.createMock(Player.class);
-//        Player mockP3 = EasyMock.createMock(Player.class);
-//        Player mockP4 = EasyMock.createMock(Player.class);
-//        Player mockP5 = EasyMock.createMock(Player.class);
-//        Card mockDefuse = EasyMock.createMock(Card.class);
-//        Card mockCard = EasyMock.createMock(Card.class);
-//
-//        EasyMock.expect(mockDeck.count()).andStubReturn(100);
-//        EasyMock.expect(mockDeck.drawDefuse()).andReturn(mockDefuse).times(5);
-//        EasyMock.expect(mockDeck.draw()).andReturn(mockCard).times(35);
-//
-//        mockP1.addCard(mockDefuse);
-//        EasyMock.expectLastCall().once();
-//        mockP1.addCard(mockCard);
-//        EasyMock.expectLastCall().times(7);
-//        mockP2.addCard(mockDefuse);
-//        EasyMock.expectLastCall().once();
-//        mockP2.addCard(mockCard);
-//        EasyMock.expectLastCall().times(7);
-//        mockP3.addCard(mockDefuse);
-//        EasyMock.expectLastCall().once();
-//        mockP3.addCard(mockCard);
-//        EasyMock.expectLastCall().times(7);
-//        mockP4.addCard(mockDefuse);
-//        EasyMock.expectLastCall().once();
-//        mockP4.addCard(mockCard);
-//        EasyMock.expectLastCall().times(7);
-//        mockP5.addCard(mockDefuse);
-//        EasyMock.expectLastCall().once();
-//        mockP5.addCard(mockCard);
-//        EasyMock.expectLastCall().times(7);
-//
-//        mockDeck.insert(EasyMock.anyObject(Card.class));
-//        EasyMock.expectLastCall().anyTimes();
-//        mockDeck.shuffle();
-//        EasyMock.expectLastCall().anyTimes();
-//
-//        EasyMock.replay(mockDeck, mockP1, mockP2, mockP3, mockP4, mockP5, mockDefuse, mockCard);
-//
-//        new Game(mockDeck, List.of(mockP1, mockP2, mockP3, mockP4, mockP5)).setup();
-//
-//        EasyMock.verify(mockDeck, mockP1, mockP2, mockP3, mockP4, mockP5);
-//    }
-//
-//    @Test
-//    public void setup_FivePlayersButNotEnoughCards_IllegalStateException() {
-//        Deck mockDeck = EasyMock.createMock(Deck.class);
-//        Player mockP1 = EasyMock.createMock(Player.class);
-//        Player mockP2 = EasyMock.createMock(Player.class);
-//        Player mockP3 = EasyMock.createMock(Player.class);
-//        Player mockP4 = EasyMock.createMock(Player.class);
-//        Player mockP5 = EasyMock.createMock(Player.class);
-//
-//        EasyMock.expect(mockDeck.count()).andStubReturn(34);
-//
-//        EasyMock.replay(mockDeck);
-//
-//        Exception exception = assertThrows(IllegalStateException.class, () -> {
-//            new Game(mockDeck, List.of(mockP1, mockP2, mockP3, mockP4, mockP5)).setup();
-//        });
-//    }
-//
+    @ParameterizedTest
+    @ValueSource(ints = {2, 3, 4, 5})
+    public void setup_ValidPlayerCounts_DealsCardsAndInsertsKittens(int playerCount) {
+        Deck mockDeck = EasyMock.createMock(Deck.class);
+        List<Player> realPlayers = new ArrayList<>();
+
+        for (int i = 0; i < playerCount; i++) {
+            realPlayers.add(new Player("Player " + (i + 1)));
+        }
+
+        EasyMock.expect(mockDeck.count()).andStubReturn(100);
+
+        mockDeck.insert(EasyMock.anyObject(Card.class), EasyMock.eq(0));
+        EasyMock.expectLastCall().times(playerCount - 1);
+
+        mockDeck.shuffle();
+        EasyMock.expectLastCall().once();
+
+        EasyMock.replay(mockDeck);
+
+        new Game(mockDeck, realPlayers).setup();
+
+        EasyMock.verify(mockDeck);
+
+        for (Player p : realPlayers) {
+            assertEquals(8, p.getCards().size(), "Player should have exactly 8 cards");
+            assertTrue(p.hasDefuse(), "Player should have received a Defuse card");
+        }
+    }
+
+    @Test
+    public void setup_FivePlayersButNotEnoughCards_IllegalStateException() {
+        Deck mockDeck = EasyMock.createMock(Deck.class);
+        List<Player> realPlayers = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            realPlayers.add(new Player("Player " + i));
+        }
+
+        EasyMock.expect(mockDeck.count()).andStubReturn(34);
+
+        EasyMock.replay(mockDeck);
+
+        Exception exception = assertThrows(IllegalStateException.class, () -> {
+            new Game(mockDeck, realPlayers).setup();
+        });
+    }
+
 //    @Test
 //    public void runGame_TwoPlayers_FirstPlayerTakesTurn() {
 //        Deck mockDeck = EasyMock.createMock(Deck.class);
