ca10f3156f85804d68b49b5b03beb2a93237749e
zsharon012 <zsharon012@gmail.com>
2026-06-10 12:24:06 -0500

    refactor to mocks: executeCardAction_oneCardDeckEmptyHand_IllegalArgumentException passes

--- src/test/java/domain/DrawTwoControllerTests.java
+++ src/test/java/domain/DrawTwoControllerTests.java
@@ -34,23 +34,25 @@ public class DrawTwoControllerTests {
 
     @Test
     public void executeCardAction_oneCardDeckEmptyHand_IllegalArgumentException() {
-        Game game = new Game(2);
-        Player initiator = game.getTotalPlayers().get(0);
+        Game mockGame = EasyMock.createMock(Game.class);
+        Player mockUser = EasyMock.createMock(Player.class);
+        Deck mockDeck = EasyMock.createMock(Deck.class);
 
-        for (int i = 0; i < 33; i++) {
-            game.getDeck().takeTopCard();
-        }
+        EasyMock.expect(mockGame.getDeck()).andReturn(mockDeck);
+        EasyMock.expect(mockDeck.count()).andReturn(1);
 
-        assertEquals(1, game.getDeck().getCards().size());
-        assertEquals(new ArrayList<Card>(), initiator.getHand());
+        EasyMock.replay(mockGame, mockUser, mockDeck);
 
         DrawTwoController controller = new DrawTwoController();
 
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            controller.executeCardAction(game, initiator, Optional.empty());
-        });
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                controller.executeCardAction(mockGame, mockUser, Optional.empty())
+        );
 
-        assertEquals("deck needs at least two cards to play draw two card", exception.getMessage());
+        assertEquals("deck needs at least two cards to play draw two card",
+                exception.getMessage());
+
+        EasyMock.verify(mockGame, mockUser, mockDeck);
     }
 
     @Test
