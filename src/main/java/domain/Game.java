b70aa01ffc6ec68feb53585b71ccd14c85997857
danielachangnu <danielachangkim2028@u.northwestern.edu>
2026-06-10 00:17:04 -0500

    spotbugs fix

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 
-public final class Game {
+public class Game {
     private final Deck deck;
     private final List<Player> totalPlayers;
     private final List<Player> alivePlayers;
