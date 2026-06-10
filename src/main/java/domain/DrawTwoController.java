c09f571f0a9a2ef10f653a8ef38a7300406b01cd
zsharon012 <zsharon012@gmail.com>
2026-06-10 13:45:50 -0500

    fix line length issue

--- src/main/java/domain/DrawTwoController.java
+++ src/main/java/domain/DrawTwoController.java
@@ -7,7 +7,8 @@ import java.util.Optional;
 public class DrawTwoController {
     public Optional<List<Card>> executeCardAction(Game game, Player user, Optional<Player> target){
         if (game.getDeck().count() < 2) {
-            throw new IllegalArgumentException("deck needs at least two cards to play draw two card");
+            String err_msg = "deck needs at least two cards to play draw two card";
+            throw new IllegalArgumentException(err_msg);
         }
 
         Card first = game.getDeck().takeTopCard();
