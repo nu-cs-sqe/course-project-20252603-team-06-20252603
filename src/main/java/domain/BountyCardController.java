68461b690fd19331f4021207eb7f3c0215378954
danielachangnu <danielachangkim2028@u.northwestern.edu>
2026-06-09 23:53:41 -0500

    refactored for clean code standards

--- src/main/java/domain/BountyCardController.java
+++ src/main/java/domain/BountyCardController.java
@@ -6,15 +6,13 @@ import java.util.Random;
 
 public class BountyCardController implements CardController {
     public Optional<List<Card>> executeCardAction(Game game, Player user, Optional<Player> target) {
-        List<Player> alive_players = game.getAlivePlayers();
-        int alive_player_count = alive_players.size();
-        for (int i = 0; i < alive_player_count; i++) {
-            Player current_player = alive_players.get(i);
-            if (current_player == user) continue;
-            if (current_player.getHandSize() > 0) {
-                List<Card> hand = current_player.getHand();
+        List<Player> alivePlayers = game.getAlivePlayers();
+        for (Player currentPlayer : alivePlayers) {
+            if (currentPlayer == user) continue;
+            if (currentPlayer.getHandSize() > 0) {
+                List<Card> hand = currentPlayer.getHand();
                 Card stolen = hand.get(new Random().nextInt(hand.size()));
-                current_player.removeCard(stolen);
+                currentPlayer.removeCard(stolen);
                 user.addCard(stolen);
             }
         }
