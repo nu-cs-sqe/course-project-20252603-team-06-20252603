96ccff5542eb91befb1fed0609b0cdf3b36fd10d
danielachangnu <danielachangkim2028@u.northwestern.edu>
2026-06-13 10:49:53 -0400

    fixed merge conflicts and updated file names

--- src/main/java/domain/FavorController.java
+++ src/main/java/domain/FavorController.java
@@ -11,7 +11,9 @@ public class FavorController implements CardController {
         this.userInput = userInput;
     }
 
-    public Optional<List<Card>> executeCardAction(GameController gameController, Player user, Optional<Player> target) {
+    public Optional<List<Card>> executeCardAction(GameController gameController,
+                                                  Player user,
+                                                  Optional<Player> target) {
         Player t = target.get();
         Card card = userInput.getCardToGive(t.getHand());
 
