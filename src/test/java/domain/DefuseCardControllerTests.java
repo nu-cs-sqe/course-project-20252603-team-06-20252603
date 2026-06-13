2883c64c5d606e8f2c2da3f148fb409868cfb8e1
danielachangnu <danielachangkim2028@u.northwestern.edu>
2026-06-12 22:42:20 -0500

    changed file name as asked

--- src/test/java/domain/DefuseCardControllerTests.java
+++ src/test/java/domain/DefuseCardControllerTests.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class DefuseControllerTests {
+public class DefuseCardControllerTests {
     @Test
     public void executeCardAction_defusePlayed_throwIllegalStateException() {
         GameController mockController = EasyMock.createMock(GameController.class);
