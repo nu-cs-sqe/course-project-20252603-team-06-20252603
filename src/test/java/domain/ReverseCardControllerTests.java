001a2602105c45a1f85d07cff5aa9fcc76954aca
AudreyPaleczny2028 <audreypaleczny2028@u.northwestern.edu>
2026-06-13 06:33:33 -0500

    removed old assertions import

--- src/test/java/domain/ReverseCardControllerTests.java
+++ src/test/java/domain/ReverseCardControllerTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class ReverseCardControllerTests {
 
