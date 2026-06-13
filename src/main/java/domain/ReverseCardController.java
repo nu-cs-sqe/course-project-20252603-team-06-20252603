f8d960232048900e757b81c8cddbed2172cbb05b
AudreyPaleczny2028 <audreypaleczny2028@u.northwestern.edu>
2026-06-13 11:39:18 -0500

    fixed variable name for clean code

--- src/main/java/domain/ReverseCardController.java
+++ src/main/java/domain/ReverseCardController.java
@@ -11,15 +11,15 @@ public class ReverseCardController implements CardController {
                                                   Player user,
                                                   Optional<Player> target) {
         List<Player> alivePlayers = gameController.getGame().getAlivePlayers();
-        int size = alivePlayers.size();
+        int playerCount = alivePlayers.size();
 
         List<Player> reversedOrder = new ArrayList<>(alivePlayers);
         Collections.reverse(reversedOrder);
         gameController.setPlayerOrder(reversedOrder);
 
-        int newCurrentIndex = size - 1 - gameController.getCurrentPlayerIndex();
+        int newCurrentIndex = playerCount - 1 - gameController.getCurrentPlayerIndex();
         gameController.setCurrentPlayerIndex(newCurrentIndex);
-        gameController.setNextPlayerIndex((newCurrentIndex + 1) % size);
+        gameController.setNextPlayerIndex((newCurrentIndex + 1) % playerCount);
 
         return Optional.empty();
     }
